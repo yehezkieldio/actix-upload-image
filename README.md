6109fcbdadd505f38ffe042b6b587e67844d5d68
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-30 00:36:46 +0800

    feat: impl

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+curl -X POST -H "Content-Type: image/png" --data-binary @Image.png http://127.0.0.1:8080/upload
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    web::{self},
+    App, HttpResponse, HttpServer, Responder,
+};
+use futures::StreamExt;
+use image::ImageFormat;
+
+async fn upload_image(mut payload: web::Payload) -> impl Responder {
+    let mut bytes = web::BytesMut::new();
+
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk.unwrap();
+        bytes.extend_from_slice(&chunk);
+    }
+
+    println!("Received {} bytes.", bytes.len());
+
+    let format = match image::guess_format(&bytes) {
+        Ok(format) => format,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return HttpResponse::BadRequest().body("Invalid image format.");
+        }
+    };
+
+    match format {
+        ImageFormat::Jpeg | ImageFormat::Png => {
+            println!("Image format: {:?}", format);
+        }
+        _ => return HttpResponse::BadRequest().body("Unsupported image format."),
+    }
+
+    let file_extension = match format {
+        ImageFormat::Jpeg => "jpg",
+        ImageFormat::Png => "png",
+        _ => unreachable!(),
+    };
+
+    let image_name = format!("{}.{}", chrono::Local::now().timestamp(), file_extension);
+    std::fs::write(format!("images/{}", image_name), &bytes).unwrap();
+
+    HttpResponse::Ok().body("Image uploaded successfully.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server.");
+
+    HttpServer::new(|| App::new().route("/upload", web::post().to(upload_image)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
